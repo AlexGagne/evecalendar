00f1973808158793b9db29868e1044540c79a40c
Alex Gagné <alex_gagne94@hotmail.com>
2017-03-30 22:57:16 -0400

    adding more haddock documentation

--- src/Eve/Internal/Types/CachedUntil.hs
+++ src/Eve/Internal/Types/CachedUntil.hs
@@ -3,5 +3,7 @@
 
 module Eve.Internal.Types.CachedUntil (CachedUntil, cachedUntil) where 
 
+-- | Implementing 'CachedUntil' will let you create types that you can use 'cachedUntil' on to get their cached time
 class CachedUntil a c | a -> c where
+  -- | 'cachedUntil' returns the time until which the data obtained will be cached 
   cachedUntil :: a -> c
--- src/Eve/Internal/Types/Credentials.hs
+++ src/Eve/Internal/Types/Credentials.hs
@@ -3,6 +3,7 @@ where
 
 import Data.Text
 
+-- | 'Credentials' represents the keyId and verification code needed to log into EVE's XML API
 data Credentials = Credentials
   { keyId :: Text
   , vCode :: Text
